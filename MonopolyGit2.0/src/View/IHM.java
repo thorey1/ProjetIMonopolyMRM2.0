f53343dd0a4c917111ae7b9932e95676fa0dfd28
boedam <boedam@pc-dg-035-07.iut2.upmf-grenoble.fr>
2018-06-20 11:12:06 +0200

    Merge origin/master
    Conflicts:
    	MonopolyGit2.0/src/Model/Propriete.java

--- MonopolyGit2.0/src/View/IHM.java
+++ MonopolyGit2.0/src/View/IHM.java
@@ -36,7 +36,7 @@ import javax.swing.border.Border;
  */
 public class IHM implements Observe {
 
-    JFrame fenetre, fenetremenu, fenetreregles;
+    JFrame fenetre, fenetremenu, fenetreregles1,fenetreregles2;
     private Observateur o;
     JComboBox listeJoueurs;
 
@@ -763,34 +763,128 @@ public class IHM implements Observe {
 
         arreter.addActionListener((ActionEvent e) -> {
             fenetremenu.setVisible(false);
+            fenetremenu.disable();
         });
 
         regles.addActionListener((ActionEvent e) -> {
             fenetremenu.setVisible(false);
-            fenetreregles.setVisible(true);
-            fenetreregles.repaint();
+            fenetreregles1.setVisible(true);
+            fenetreregles1.repaint();
         });
 
-        //fenetre regles
-        fenetreregles = new JFrame();
-        fenetreregles.setTitle("Règles Monopoly");
-        fenetreregles.setSize(400, 200);
-        fenetreregles.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetreregles.setVisible(false);
+        //fenetre regles1
+        fenetreregles1 = new JFrame();
+        fenetreregles1.setTitle("Règles Monopoly");
+        fenetreregles1.setSize(600, 950);
+        fenetreregles1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetreregles1.setVisible(false);
 
         JLabel teteRegles = new JLabel("Règles du Monopoly");
-        fenetreregles.add(teteRegles, BorderLayout.NORTH);
+        fenetreregles1.add(teteRegles, BorderLayout.NORTH);
         teteRegles.setHorizontalAlignment(JLabel.CENTER);
-        JButton retour = new JButton("Retour au menu");
-        fenetreregles.add(retour, BorderLayout.SOUTH);
+        
+        JLabel regle1 = new JLabel("<html> <h2>I.Préparation</h2> "
+                + "<p>1. Chaque joueur choisit un pion et le place sur la case numéro 1. </p> <br>"
+                + "<p>2. Le  Banquier  remet  à  chaque  joueur  une  somme  de 1500  euros. La  banque  possède  les Titres de  Propriété ."
+                + "La  Banque  encaisse  l'argent de  la  vente  des  titres  de  propriété. La  Banque  ne peut jamais faire faillite(on supposera"
+                + "qu'elle détient une quantité infinie d'argent)</p> <br>"
+                + "<p>3. Les joueurs jouent dans l'ordre de leur placement.</p> <br>"
+                + "<h2>II.Déroulement d'un tour de jeu</h2>"
+                + "<p>Quand arrive votre tour de jeu, lancez les 2 dés puis avancez votre pion sur le plateau de jeu, d'autant de cases que l'indique"
+                + "le total des dés, et dans le sens indiqué par la flèche. La case sur laquelle vous vous arrêtez vous indique ce que vous devez ou pouvez"
+                + "faire. Deux pions ou plus peuvent se trouver sur la même case a même moment. Suivant la nature de la case sur laquelle vous êtes arrivé, "
+                + "vous pouvez effectuer l'une des opérations suivantes : <br>"
+                + "<b>. acheter un terrain ou une autre propriété <br>"
+                + ". payer un loyer (si la propriété appartient à un autre joueur)</b><br>"
+                + "Lorsque vous avez terminé votre tour, ce sera au joueur situé à votre gauche de joueur.</p>"
+                + "<h4>1. Les doubles</h4>"
+                + "<p>Si  vous  obtenez  un  double  avec  les  dés, déplacez vous  normalement.  Effectuez  l'opération  de votre case d'arrivée, puis "
+                + "relancez les dés et déplacez vous à nouveau. Vous effectuerez alors une nouvelle opération. </p> <br>"
+                + "<h4>2. Arrêt sur une propriété à vendre</h4>"
+                + "<p>Si vous vous arrêtez sur une propriété qui n'appartient à personne (c'est à dire une propriété dont aucun  joueur  ne  possède  le"
+                + "titre  de propriété),  vous  pouvez  décider  de  l'acheter.  Dans  ce cas, payez à la Banque le prix indiqué sur le plateau."
+                + " La Banque vous donnera alors, en échange, le titre de propriété correspondant.</p> <br>"
+                + "<h4>3. Posséder une propriété</h4>"
+                + "<p>Posséder une propriété vous donne le droit de percevoir un loyer de la part des joueurs qui vont s'arrêter sur cette propriété.</p><br>"
+                + "<h4>4. Arrêt sur une propriété privée</h4>"
+                + "<p>Quand  vous  vous  arrêtez  sur  une  Propriété  qui  a  été  achetée  par  un  autre  joueur, vous devez payer  un  loyer  au "
+                + "propriétair.  Le  montant  du  loyer  figure  sur  le titre  de propriété.  Si  un  joueur possède tous les terrains d'une même"
+                + " couleur, la valeur du loyer du terrain nu est alors multipliée par deux. </p> <br>"
+                + "<h4>5. Arrêt sur les cases COMPAGNIE DE DISTRIBUTION</h4>"
+                + "<p>Quand  vous  vous  arrêtez  sur  une  de  ces  cases,  vous  pouvez  l'acheter  si  cette  Compagnie  de Distribution  n'appartient"
+                + "  à  personne.  Comme  pour  les  Propriétés,  payez  à  la  Banque  le  prix indiqué  sur  le  plateau.  Si  la  Compagnie  de "
+                + " Distribution  appartient  déjà  à  l'un  des  joueurs, vous devez payer un loyer, qui est fonction des points indiqués par"
+                + " vos dés :</p> <br></html>");
+        fenetreregles1.add(regle1,BorderLayout.CENTER);
+        
 
+        
+        JButton retour = new JButton("Retour au menu");
+        JButton nextpage = new JButton("Page Suivante");
+        JPanel pbas = new JPanel(new GridLayout(0,2));
+        pbas.add(retour);
+        pbas.add(nextpage);
+        fenetreregles1.add(pbas, BorderLayout.SOUTH);
+        
+        //fenetre regles2
+        fenetreregles2 = new JFrame();
+        fenetreregles2.setTitle("Règles Monopoly (suite)");
+        fenetreregles2.setSize(600, 500);
+        fenetreregles2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetreregles2.setVisible(false);
+        
+        JLabel teteRegles2 = new JLabel("Règles du Monopoly (suite)");
+        fenetreregles2.add(teteRegles2, BorderLayout.NORTH);
+        teteRegles2.setHorizontalAlignment(JLabel.CENTER);
+        
+        
+        JLabel regle2 = new JLabel("<html><p>Si ce propriétaire possède une Compagnie de Distribution, le loyer sera égal à quatre fois"
+                + " la valeur de vos dés</p> <br>"
+                + "<p>S'il possède les deux Compagnies de Distribution, le loyer s'élèvera alors à dix fois la valeur indiquée par les dés.</p> <br>"
+                + "<h4>6. Arrêt sur une gare</h4>"
+                + "<p>Si vous êtes le premier joueur à vous arrêter sur une case Gare, vous pouvez décider de l'acheter. Si  la gare a  déjà  "
+                + "été  achetée,  vous devez  payer au propriétaire  le montant  indiqué  sur  le titre  de propriété. Le loyer varie en "
+                + "fonction du nombre total de Gares possédées par ce joueur.</p>< <br>"
+                + "<h4>7. Faillite</h4>"
+                + "<p>Si vous ne possédez plus d'argent, vous êtes en faillite et vous devez alors vous retirer du jeu. Tous vos  biens "
+                + " (argent,  titres de propriété)  sont  remis à la  banque.  Les  titres  de  propriété  sont  à nouveau disponibles dans"
+                + " les conditions initiales du jeu.</p> <br>"
+                + "<h4>8. Le vainqueur</h4>"
+                + "<p>Le dernier joueur restant en jeu est le vainqueur.</p></html>");
+        fenetreregles2.add(regle2,BorderLayout.CENTER);
+        
+        
+        
+        JButton retour2 = new JButton("Retour au menu");
+        JButton retourpage = new JButton("Page précédente");
+        JPanel pbas2 = new JPanel(new GridLayout(0,2));
+        pbas2.add(retourpage);
+        pbas2.add(retour2);
+        fenetreregles2.add(pbas2, BorderLayout.SOUTH);
+        
+        
         //Listener regles
         retour.addActionListener((ActionEvent e) -> {
-            fenetreregles.setVisible(false);
+            fenetreregles1.setVisible(false);
+            fenetreregles2.setVisible(false);
             fenetremenu.setVisible(true);
             fenetremenu.repaint();
         });
-
+        retour2.addActionListener((ActionEvent e) -> {
+            fenetreregles1.setVisible(false);
+            fenetreregles2.setVisible(false);
+            fenetremenu.setVisible(true);
+            fenetremenu.repaint();
+        });
+        nextpage.addActionListener((ActionEvent e) -> {
+            fenetreregles1.setVisible(false);
+            fenetreregles2.setVisible(true);
+            fenetreregles2.repaint();
+        });
+        retourpage.addActionListener((ActionEvent e) ->{
+            fenetreregles2.setVisible(false);
+            fenetreregles1.setVisible(true);
+        });
     }
 
     public void fenetrePlateau() {
